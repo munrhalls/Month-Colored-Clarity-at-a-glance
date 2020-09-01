89b7233dbf9f495aa598b0f69c3da9bc52163e6b
munrhalls <antarcticdepths@gmail.com>
2020-09-01 14:03:04 +0200

    Turn array of week day names -> to day objects with name: '..', count: '...'

--- script.js
+++ script.js
@@ -17,18 +17,29 @@ window.addEventListener('load', function () {
   week.style.display = 'inline-block';
   document.getElementById('root').appendChild(week);
   // 2. CREATE DAYS DIVS
-  var daysDATA = ['Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  var daysDATA = [
+    {name: 'Monday', count: 0},
+    {name: 'Tuesday', count: 0}, 
+    {name: 'Wendesday', count: 0},
+    {name: 'Thursday', count: 0},
+    {name: 'Friday', count: 0},
+    {name: 'Saturday', count: 0},
+    {name: 'Sunday', count: 0},
+  ];
+    
   daysDATA.forEach(function(el){
     var day = document.createElement('div');
-    day.innerText = el;
+    day.innerText = el['name'];
     //style
     day.style.border = '1px solid gray'; day.style.padding = '3px 30px'; 
     day.style.display = 'inline-block'; 
     week.appendChild(day);
-  })
+  });
+  
+  var hoursCount = 
 
-
-  // 5. CREATE TICKS 
+  // 3. CREATE TICKS 
 
   // for (var i = 0; i < 100; i++) {
   //   var div = document.createElement('div');
