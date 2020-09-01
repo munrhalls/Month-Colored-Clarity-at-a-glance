bef59507ff9999bf52bdbaa0cba897ae235241bc
munrhalls <antarcticdepths@gmail.com>
2020-09-01 10:57:46 +0200

    add a 100 divs on page load, border white, looks like table

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
+  
+  for (var i = 0; i < 100; i++) {
+    var div = document.createElement('div');
+    div.style.border = "1px solid white";
+    div.style.margin = '15px';
+    document.getElementById('root').appendChild(div);
+  }
+
 });
+
+
