3dcc2555e24c17a65a820f36a186e34ba53bf2be
munrhalls <antarcticdepths@gmail.com>
2020-09-01 12:28:11 +0200

    Fill svg background rect color with js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
-  
-  for (var i = 0; i < 100; i++) {
-    var div = document.createElement('div');
-    div.style.border = "1px solid white";
-    div.style.margin = '15px';
-    document.getElementById('root').appendChild(div);
-  }
+  // for (var i = 0; i < 100; i++) {
+  //   var div = document.createElement('div');
+  //   div.style.border = "1px solid white";
+  //   div.style.margin = '15px';
+  //   document.getElementById('root').appendChild(div);
+  // }
 
+  var svgBackground = document.getElementById('canvas_background');
+  var y = parseFloat(svgBackground.getAttributeNS(null, 'y'));
+  svgBackground.setAttributeNS(null, 'fill', 'grey');
 });
 
 
