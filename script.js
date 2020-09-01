94d3c308dc59f83eced71d8636be95e5b61f613f
munrhalls <antarcticdepths@gmail.com>
2020-09-01 13:22:23 +0200

    RESET COMMIT - just create an svg circle with js only

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var moveSlider = function(slider, direction) {
-  var value = slider.value;
-  console.log(value)
-  var circle = document.getElementById("target");
-  var coord = "c" + direction;
-  circle.setAttributeNS(null, coord, value * 5);
-}
+
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
+  var svg = document.getElementsByTagName('svg')[0];
+  svg.setAttribute('height', 300)
+  var element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  element.setAttribute('cy', '100');
+  element.setAttribute('cx', '100');
+  element.setAttribute('r', '100');
+  svg.appendChild(element);
+
   // for (var i = 0; i < 100; i++) {
   //   var div = document.createElement('div');
   //   div.style.border = "1px solid white";
