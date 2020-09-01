3e485927f699d119659f8e5ff2e79452642ecd1c
munrhalls <antarcticdepths@gmail.com>
2020-09-01 13:38:39 +0200

    all else out; create week div

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
-  var svg = document.getElementsByTagName('svg')[0];
-  svg.setAttribute('height', 300)
-  var element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  element.setAttribute('cy', '100');
-  element.setAttribute('cx', '100');
-  element.setAttribute('r', '100');
-  svg.appendChild(element);
+  var week = document.createElement('div');
+  week.className = 'week';
+  week.innerText = 'Week';
+  week.style.border = '1px solid grey';
+  week.style.display = 'inline-block';
+  document.getElementById('root').appendChild(week);
+  
+
 
   // for (var i = 0; i < 100; i++) {
   //   var div = document.createElement('div');
