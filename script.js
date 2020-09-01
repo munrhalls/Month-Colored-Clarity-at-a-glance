3d13376e592cf56cd8747032e52b0355c6292cdb
munrhalls <antarcticdepths@gmail.com>
2020-09-01 13:52:28 +0200

    add hours column

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
+  // 0. CREATE DEEP WORK HOURS COLUMN TO THE LEFT
+  var hours = document.createElement('div');
+  hours.className = 'hours'; 
+  hours.innerText ='Hours'
+  hours.style.border = '1px solid gray';   hours.style.padding = '3px 30px'; 
+  hours.style.display = 'inline-block';
+  document.getElementById('root').appendChild(hours);
   // 1. CREATE WEEK DIV
   var week = document.createElement('div');
-  week.className = 'week'; week.style.border = '1px solid grey';
+  week.className = 'week';
   week.style.display = 'inline-block';
   document.getElementById('root').appendChild(week);
   // 2. CREATE DAYS DIVS
@@ -12,12 +19,14 @@ window.addEventListener('load', function () {
   daysDATA.forEach(function(el){
     var day = document.createElement('div');
     day.innerText = el;
+    //style
     day.style.border = '1px solid gray'; day.style.padding = '3px 30px'; 
     day.style.display = 'inline-block'; 
     week.appendChild(day);
   })
 
 
+  // 5. CREATE TICKS 
 
   // for (var i = 0; i < 100; i++) {
   //   var div = document.createElement('div');
