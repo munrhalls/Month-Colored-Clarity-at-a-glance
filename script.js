c8fab9d8c40c2cfe1cf910f5b5819f36266c9692
munrhalls <antarcticdepths@gmail.com>
2020-09-01 19:54:42 +0200

    MINI MILESTONE COMPLETE - USER CLICKS BTN- > DEEP WORK HOURS CIRCLES ADD UP IN THE GIVEN DAY'S COLUMN

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
-
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
   // 0. CREATE DEEP WORK HOURS COLUMN TO THE LEFT
   var hours = document.createElement('div');
-  hours.className = 'hours'; 
-  hours.innerText ='Hours'
-  hours.style.color = "#fff";
-  hours.style.border = '1px solid gray';   hours.style.padding = '3px 30px'; 
-  hours.style.display = 'inline-block';
-  hours.style.height = '100vh'; hours.style.backgroundColor = '#000';
+    hours.className = 'hours'; 
+    hours.innerText ='Hours'
+    hours.style.color = "#fff";
+    hours.style.border = '1px solid gray';   hours.style.padding = '3px 30px'; 
+    hours.style.display = 'inline-block';
+    hours.style.height = '100vh'; hours.style.backgroundColor = '#000';
   document.getElementById('root').appendChild(hours);
   // 1. CREATE WEEK DIV
   var week = document.createElement('div');
-  week.className = 'week';
-  week.style.display = 'inline-block';
+    week.className = 'week';
+    week.style.display = 'inline-block';
   document.getElementById('root').appendChild(week);
   // 2. CREATE DAYS DIVS
   var daysDATA = [
@@ -30,32 +28,31 @@ window.addEventListener('load', function () {
   daysDATA.forEach(function(el){
     //day div
     var day = document.createElement('div');
-    day.innerText = el['name'];
-    //style
-    day.style.border = '1px solid gray'; day.style.padding = '3px 30px'; 
+      day.innerText = el['name'];
+      //style
+      day.style.border = '1px solid gray'; day.style.padding = '3px 30px'; 
     var dayContainer = document.createElement('div');
-    dayContainer.appendChild(day);
-    dayContainer.style.display = 'inline-block'; 
-    week.appendChild(dayContainer);
+      dayContainer.appendChild(day);
+      dayContainer.style.display = 'inline-block'; 
+      week.appendChild(dayContainer);
     //3. APPEND ADD HOUR TICKS BUTTON TO EACH DAY
     var button = document.createElement('button');
-    button.style.width = "15px";
-    button.style.height = '15px';
-    button.onclick = (function(){
-      var parent = button.parentNode;
+      button.style.width = "15px";
+      button.style.height = '15px';
+      button.onclick = (function(){
+    var parent = button.parentNode;
       // 4. (REPRESENT TICKS DATA) - CREATE DIVS FOR HOUR TICKS 
-      var svg = document. createElementNS("http://www.w3.org/2000/svg", "svg");
+    var svg = document. createElementNS("http://www.w3.org/2000/svg", "svg");
       svg.setAttribute("width", "100%");
       svg.setAttribute("height", "28");
       svg.setAttribute('display', 'block');
       svg.style.marginTop = '7px';
-      var tick = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    var tick = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       tick.setAttributeNS(null, 'cy', '50%');
       tick.setAttributeNS(null, 'cx', '50%');
       tick.setAttributeNS(null, 'r', '14');
       tick.setAttributeNS(null, 'fill', '#000');
       svg.appendChild(tick);
-
       parent.appendChild(svg);
     });
     
