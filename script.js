d095f68b46348cf82ee4ad2c3d41c7d60e625cee
munrhalls <antarcticdepths@gmail.com>
2020-09-02 09:49:46 +0200

    add tick svg using path d (still pure js)

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ window.addEventListener('load', function () {
       // input.style.height = '15px';
       input.style.hover = "cursor";
       input.setAttribute('type', 'button');
+      input.style.marginLeft = "auto";
       input.setAttribute('value', '+');
       input.onclick = (function(){
     var parent = input.parentNode;
@@ -50,11 +51,11 @@ window.addEventListener('load', function () {
       svg.setAttribute("height", "28");
       svg.setAttribute('display', 'block');
       svg.style.marginTop = '7px';
-    var tick = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-      tick.setAttributeNS(null, 'cy', '50%');
-      tick.setAttributeNS(null, 'cx', '50%');
-      tick.setAttributeNS(null, 'r', '14');
-      tick.setAttributeNS(null, 'fill', '#000');
+    var tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      tick.setAttributeNS(null, 'd', 'M95.833,0C42.991,0,0,42.99,0,95.833s42.991,95.834,95.833,95.834s95.833-42.991,95.833-95.834S148.676,0,95.833,0z   M150.862,79.646l-60.207,60.207c-2.56,2.56-5.963,3.969-9.583,3.969c-3.62,0-7.023-1.409-9.583-3.969l-30.685-30.685  c-2.56-2.56-3.97-5.963-3.97-9.583c0-3.621,1.41-7.024,3.97-9.584c2.559-2.56,5.962-3.97,9.583-3.97c3.62,0,7.024,1.41,9.583,3.971  l21.101,21.1l50.623-50.623c2.56-2.56,5.963-3.969,9.583-3.969c3.62,0,7.023,1.409,9.583,3.969  C156.146,65.765,156.146,74.362,150.862,79.646z');
+      // tick.setAttributeNS(null, 'cx', '50%');
+      // tick.setAttributeNS(null, 'r', '14');
+      // tick.setAttributeNS(null, 'fill', '#000');
       svg.appendChild(tick);
       parent.appendChild(svg);
     });
