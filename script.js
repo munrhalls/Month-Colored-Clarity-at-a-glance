23ea76bb9cf9dc92e0fd1ebbf61541cd9fd04d2a
munrhalls <antarcticdepths@gmail.com>
2020-09-02 10:38:15 +0200

    vastly improved the look - added padding on svgDiv, added bg color as deep blue

--- script.js
+++ script.js
@@ -49,12 +49,14 @@ window.addEventListener('load', function () {
         var parent = input.parentNode;
           // 4. (REPRESENT TICKS DATA) - CREATE DIVS FOR HOUR TICKS 
         var svgDiv = document.createElement('div');
+        svgDiv.style.backgroundColor = "#000071";
+        svgDiv.style.padding = "7px 0";
         var svg = document. createElementNS("http://www.w3.org/2000/svg", "svg");
           svg.setAttribute("width", "100%");
           svg.setAttribute("height", "28");
           svg.setAttribute("width", "28");
           svg.setAttribute('display', 'block');
-          svg.style.margin = "7px auto 0";
+          svg.style.margin = "auto";
         var tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
           tick.setAttributeNS(null, 'd', 'M95.833,0C42.991,0,0,42.99,0,95.833s42.991,95.834,95.833,95.834s95.833-42.991,95.833-95.834S148.676,0,95.833,0z   M150.862,79.646l-60.207,60.207c-2.56,2.56-5.963,3.969-9.583,3.969c-3.62,0-7.023-1.409-9.583-3.969l-30.685-30.685  c-2.56-2.56-3.97-5.963-3.97-9.583c0-3.621,1.41-7.024,3.97-9.584c2.559-2.56,5.962-3.97,9.583-3.97c3.62,0,7.024,1.41,9.583,3.971  l21.101,21.1l50.623-50.623c2.56-2.56,5.963-3.969,9.583-3.969c3.62,0,7.023,1.409,9.583,3.969  C156.146,65.765,156.146,74.362,150.862,79.646z');
           tick.style.scale = "0.14";
