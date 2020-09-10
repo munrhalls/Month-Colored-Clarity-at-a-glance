edce821e1ecdbf38462931f3c5dba1311932650c
munrhalls <antarcticdepths@gmail.com>
2020-09-11 00:28:48 +0200

    make 2nd copyright create func

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function () {
     createMonths(i);
   }
   createCopyrightNote();
+  createCopyrightNote2();
 });
 
 function createMenu() {
@@ -85,7 +86,7 @@ function createSaveDATABtn() {
   });
 } 
 function createFillDataBtn() {
-  const saveDATABtnContainer = (function createSaveDataBtnContainer() {
+  const saveDATABtnContainer = (function createFillDataContainer() {
     const saveDATABtnContainer = document.createElement('div');
       saveDATABtnContainer.style.border = "none";
       saveDATABtnContainer.style.fontWeight = "bold";
@@ -107,7 +108,8 @@ function createFillDataBtn() {
 
 const saveDATABtn = document.createElement('input');
     saveDATABtn.setAttribute('type', 'button');
-    saveDATABtn.setAttribute('value', 'SAVE \nDATA');
+
+    saveDATABtn.setAttribute('value', 'FILL \nDATA');
     saveDATABtn.style.padding = '0.15rem 1rem'
 
 const saveIcon = resourceCreateSaveIcon();
@@ -483,6 +485,10 @@ function createMonths(monthNum) {
 }
 
 // RESOURCES (graphics and so on)
+
+
+
+
 function resourceCreateSaveIcon() {
   const saveIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     saveIcon.setAttributeNS(null, 'height', '24');
@@ -507,9 +513,34 @@ function resourceCreateSaveIcon() {
   saveIcon.appendChild(path4);
   return saveIcon;
 }
+function resourceFillIcon() {
+  
+  const saveIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  saveIcon.setAttributeNS(null, 'height', '24');
+  saveIcon.setAttributeNS(null, 'width', '24');
+  // saveIcon.setAttributeNS(null, 'enableBackground', 'new 0 0 24 24');
+  saveIcon.setAttributeNS(null, 'viewBox', '0 0 24 24');
+  saveIcon.style.fill = '#fff';
+  saveIcon.style.marginTop = '0.5rem'
+
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'm21.5 20h-2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h2c.827 0 1.5-.673 1.5-1.5v-11c0-.827-.673-1.5-1.5-1.5h-11.5c-.133 0-.26-.053-.354-.146l-1.853-1.854h-5.293c-.827 0-1.5.673-1.5 1.5v13c0 .827.673 1.5 1.5 1.5h2c.276 0 .5.224.5.5s-.224.5-.5.5h-2c-1.379 0-2.5-1.122-2.5-2.5v-13c0-1.378 1.121-2.5 2.5-2.5h5.5c.133 0 .26.053.354.146l1.853 1.854h11.293c1.379 0 2.5 1.122 2.5 2.5v11c0 1.378-1.121 2.5-2.5 2.5z');
+  const path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path2.setAttribute('d', 'm12 18.75c-.276 0-.5-.224-.5-.5v-7.75c0-.276.224-.5.5-.5s.5.224.5.5v7.75c0 .277-.224.5-.5.5z');
+  const path3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path3.setAttribute('d', 'm12 19c-.128 0-.256-.049-.354-.146l-2.5-2.5c-.195-.195-.195-.512 0-.707s.512-.195.707 0l2.147 2.146 2.146-2.146c.195-.195.512-.195.707 0s.195.512 0 .707l-2.5 2.5c-.097.097-.225.146-.353.146z');
+  const path4 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path4.setAttribute('d', 'm15.5 22h-7c-.827 0-1.5-.673-1.5-1.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .276.225.5.5.5h7c.275 0 .5-.224.5-.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .827-.673 1.5-1.5 1.5z');
+
+  saveIcon.appendChild(path);
+  saveIcon.appendChild(path2);
+  saveIcon.appendChild(path3);
+  saveIcon.appendChild(path4);
+  return saveIcon; 
+}
 function createCopyrightNote() {
   const copyrightNote = document.createElement('div');
-  copyrightNote.id = 'copyright';
+  copyrightNote.classList.add('copyright');
   copyrightNote.innerText = 'Icons made by ';
   const link = document.createElement('a');
     link.href = 'https://icon54.com/';
@@ -533,4 +564,33 @@ function createCopyrightNote() {
   copyrightNote.style.fontSize = '10px'
   copyrightNote.style.textAlign = 'center';
   document.getElementById('root').appendChild(copyrightNote);
+}
+
+function createCopyrightNote2() {
+    // <a target="_blank" href="https://www.iconbros.com/icons/ib-o-f-analysis">Analysis</a> icon by <a target="_blank" href="https://iconbros.com">IconBros</a>
+  const copyrightNote2 = document.createElement('div');
+  copyrightNote2.classList.add('copyright');
+  copyrightNote2.innerText = 'Analysis ';
+  const link = document.createElement('a');
+    link.href = 'https://www.iconbros.com/icons/ib-o-f-analysis/';
+    link.title = 'Pixel perfect';
+    link.innerText = 'Pixel perfect';
+    copyrightNote2.appendChild(link);
+  const betweenLinksText = document.createElement('span');
+    betweenLinksText.innerText = ' from ';
+    copyrightNote2.appendChild(betweenLinksText);
+  const linkTwo = document.createElement('a');
+    linkTwo.href = 'https://www.flaticon.com/';
+    linkTwo.title = 'Flaticon';
+    linkTwo.innerText = 'www.flaticon.com';
+  copyrightNote2.appendChild(linkTwo);
+  const dot = document.createElement('span');
+    dot.innerText = '.';
+    dot.style.color = 'blue';
+    copyrightNote2.appendChild(dot);
+  copyrightNote2.style.backgroundColor = '#000';
+  copyrightNote2.style.color = '#fff';
+  copyrightNote2.style.fontSize = '10px'
+  copyrightNote2.style.textAlign = 'center';
+  document.getElementById('root').appendChild(copyrightNote2);
 }
