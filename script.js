7601f85eacee2819b67f17fb9c6569fcf85dee55
munrhalls <antarcticdepths@gmail.com>
2020-09-10 19:06:18 +0200

    abstract creating svg func out of the createSaveDATABtn func

--- script.js
+++ script.js
@@ -52,25 +52,9 @@ function createSaveDATABtn() {
       saveDATABtn.setAttribute('value', 'SAVE \nDATA');
       saveDATABtn.style.padding = '0.15rem 1rem'
 
-  const saveHoursBtnSymbol = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    saveHoursBtnSymbol.setAttributeNS(null, 'height', '24');
-    saveHoursBtnSymbol.setAttributeNS(null, 'width', '24');
-    // saveHoursBtnSymbol.setAttributeNS(null, 'enableBackground', 'new 0 0 24 24');
-    saveHoursBtnSymbol.setAttributeNS(null, 'viewBox', '0 0 24 24');
-    saveHoursBtnSymbol.style.fill = '#fff';
-    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', 'm21.5 20h-2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h2c.827 0 1.5-.673 1.5-1.5v-11c0-.827-.673-1.5-1.5-1.5h-11.5c-.133 0-.26-.053-.354-.146l-1.853-1.854h-5.293c-.827 0-1.5.673-1.5 1.5v13c0 .827.673 1.5 1.5 1.5h2c.276 0 .5.224.5.5s-.224.5-.5.5h-2c-1.379 0-2.5-1.122-2.5-2.5v-13c0-1.378 1.121-2.5 2.5-2.5h5.5c.133 0 .26.053.354.146l1.853 1.854h11.293c1.379 0 2.5 1.122 2.5 2.5v11c0 1.378-1.121 2.5-2.5 2.5z');
-    var path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path2.setAttribute('d', 'm12 18.75c-.276 0-.5-.224-.5-.5v-7.75c0-.276.224-.5.5-.5s.5.224.5.5v7.75c0 .277-.224.5-.5.5z');
-    var path3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path3.setAttribute('d', 'm12 19c-.128 0-.256-.049-.354-.146l-2.5-2.5c-.195-.195-.195-.512 0-.707s.512-.195.707 0l2.147 2.146 2.146-2.146c.195-.195.512-.195.707 0s.195.512 0 .707l-2.5 2.5c-.097.097-.225.146-.353.146z');
-    var path4 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path4.setAttribute('d', 'm15.5 22h-7c-.827 0-1.5-.673-1.5-1.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .276.225.5.5.5h7c.275 0 .5-.224.5-.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .827-.673 1.5-1.5 1.5z');
-
-    saveHoursBtnSymbol.appendChild(path);
-    saveHoursBtnSymbol.appendChild(path2);
-    saveHoursBtnSymbol.appendChild(path3);
-    saveHoursBtnSymbol.appendChild(path4);
+  
+  const saveHoursBtnSymbol = createSaveIcon();
+    
     saveHoursBtnSymbol.style.marginTop = '0.5rem'
 
     saveDATABtnContainer.appendChild(saveDATABtn);
@@ -425,6 +409,29 @@ function createMonths(monthNum) {
   createWeek(2, monthNum);
   createWeek(3, monthNum);
   createWeek(4, monthNum);
+}
+// RESOURCES (graphics and so on)
+function createSaveIcon() {
+  const saveHoursBtnSymbol = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    saveHoursBtnSymbol.setAttributeNS(null, 'height', '24');
+    saveHoursBtnSymbol.setAttributeNS(null, 'width', '24');
+    // saveHoursBtnSymbol.setAttributeNS(null, 'enableBackground', 'new 0 0 24 24');
+    saveHoursBtnSymbol.setAttributeNS(null, 'viewBox', '0 0 24 24');
+    saveHoursBtnSymbol.style.fill = '#fff';
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'm21.5 20h-2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h2c.827 0 1.5-.673 1.5-1.5v-11c0-.827-.673-1.5-1.5-1.5h-11.5c-.133 0-.26-.053-.354-.146l-1.853-1.854h-5.293c-.827 0-1.5.673-1.5 1.5v13c0 .827.673 1.5 1.5 1.5h2c.276 0 .5.224.5.5s-.224.5-.5.5h-2c-1.379 0-2.5-1.122-2.5-2.5v-13c0-1.378 1.121-2.5 2.5-2.5h5.5c.133 0 .26.053.354.146l1.853 1.854h11.293c1.379 0 2.5 1.122 2.5 2.5v11c0 1.378-1.121 2.5-2.5 2.5z');
+  const path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path2.setAttribute('d', 'm12 18.75c-.276 0-.5-.224-.5-.5v-7.75c0-.276.224-.5.5-.5s.5.224.5.5v7.75c0 .277-.224.5-.5.5z');
+  const path3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path3.setAttribute('d', 'm12 19c-.128 0-.256-.049-.354-.146l-2.5-2.5c-.195-.195-.195-.512 0-.707s.512-.195.707 0l2.147 2.146 2.146-2.146c.195-.195.512-.195.707 0s.195.512 0 .707l-2.5 2.5c-.097.097-.225.146-.353.146z');
+  const path4 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path4.setAttribute('d', 'm15.5 22h-7c-.827 0-1.5-.673-1.5-1.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .276.225.5.5.5h7c.275 0 .5-.224.5-.5v-2c0-.276.224-.5.5-.5s.5.224.5.5v2c0 .827-.673 1.5-1.5 1.5z');
+
+  saveHoursBtnSymbol.appendChild(path);
+  saveHoursBtnSymbol.appendChild(path2);
+  saveHoursBtnSymbol.appendChild(path3);
+  saveHoursBtnSymbol.appendChild(path4);
+  return saveHoursBtnSymbol;
 }
 function createCopyrightNote() {
   const copyrightNote = document.createElement('div');
