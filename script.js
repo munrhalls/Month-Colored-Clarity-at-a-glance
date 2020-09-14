f8266a8b14f44515d21d79b80015e5e1118eb37c
munrhalls <antarcticdepths@gmail.com>
2020-09-14 14:04:10 +0200

    abstracted addHourNum func works, fix css

--- script.js
+++ script.js
@@ -345,15 +345,15 @@ function createHourTick() {
     return hour;
 };
 function appendNumToHour(num, hour) {
-  var hourCount = document.createElement('div');
-  hourCount.innerText = num;
-  hourCount.style.color = "#000";
-  hourCount.style.fontWeight = "bold";
-  hourCount.style.fontSize = '1.25rem';
-  hourCount.style.position = 'absolute';
-  hourCount.style.left = '15px';
-  hour.appendChild(hourCount);
-  return hour;
+  var numEl = document.createElement('div');
+  numEl.style.color = '#000';
+  numEl.style.fontWeight = "bold";
+  numEl.style.fontSize = '1.6rem';
+  numEl.style.position = 'absolute';
+  numEl.style.left = '15px';
+  numEl.style.top = '0.1rem';
+  numEl.innerText = num;
+  hour.appendChild(numEl);
 }
 function createBtns(dayContainer) {
   var addBtn = document.createElement('input');
@@ -377,40 +377,8 @@ function createBtns(dayContainer) {
         hourNum++;
         const hour = createHourTick();
         appendNumToHour(hourNum, hour);
-        
         var parent = addBtn.parentNode;
-
-        var svgDiv = document.createElement('div');
-        svgDiv.className = "hour";
-        svgDiv.style.backgroundColor = "#000071";
-        svgDiv.style.padding = "7px 0";
-        svgDiv.style.border = "1px solid darkblue";
-        svgDiv.style.position = 'relative';
-        //deep work hour count- visual representer
-        var hourCount = document.createElement('div');
-        ///////////////////////////////////////////
-
-        hourCount.style.color = "#000";
-        hourCount.style.fontWeight = "bold";
-        hourCount.style.fontSize = '1.25rem';
-        hourCount.style.position = 'absolute';
-        hourCount.style.left = '15px';
-        svgDiv.appendChild(hourCount);
-        // deep work hour count - visual representer
-        var svg = document. createElementNS("http://www.w3.org/2000/svg", "svg");
-          svg.setAttribute("width", "100%");
-          svg.setAttribute("height", "28");
-          svg.setAttribute("width", "28");
-          svg.setAttribute('display', 'block');
-          svg.style.margin = "auto";
-        var tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-          tick.setAttributeNS(null, 'd', 'M95.833,0C42.991,0,0,42.99,0,95.833s42.991,95.834,95.833,95.834s95.833-42.991,95.833-95.834S148.676,0,95.833,0z   M150.862,79.646l-60.207,60.207c-2.56,2.56-5.963,3.969-9.583,3.969c-3.62,0-7.023-1.409-9.583-3.969l-30.685-30.685  c-2.56-2.56-3.97-5.963-3.97-9.583c0-3.621,1.41-7.024,3.97-9.584c2.559-2.56,5.962-3.97,9.583-3.97c3.62,0,7.024,1.41,9.583,3.971  l21.101,21.1l50.623-50.623c2.56-2.56,5.963-3.969,9.583-3.969c3.62,0,7.023,1.409,9.583,3.969  C156.146,65.765,156.146,74.362,150.862,79.646z');
-          tick.style.scale = "0.14";
-          tick.style.x = "50%";
-          svg.appendChild(tick);
-          svgDiv.appendChild(svg);
-          ///////////////////////////////////////////
-          parent.appendChild(svgDiv);
+        parent.appendChild(hour);
       };
       dayContainer.appendChild(addBtn);
       //6. APPEND SUBSTRACT HOURS BUTTON TO EACH DAY 
