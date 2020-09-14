261548e7e29fce9a72daba11f9576ae00d3c70ed
munrhalls <antarcticdepths@gmail.com>
2020-09-14 17:16:45 +0200

    simplify code - abstract createAsTextFile from the callback

--- script.js
+++ script.js
@@ -98,30 +98,33 @@ function createSaveTextFileBtn() {
 }
 function saveHoursAsTextFile() {
   // the only data it needs = hour ticks numbers 
-   const days = document.getElementsByClassName('day');
-   const hourNums = [];
-   for (let i = 0; i < days.length; i++) {
-     hourNums.push(days[i].getElementsByClassName('hour').length);
-   }
-   const hourNumsStr = hourNums.join(','); 
-      
-   function createTextFile(filename, data) {
-     var blob = new Blob([data], {type: 'text'});
-     if(window.navigator.msSaveOrOpenBlob) {
-         window.navigator.msSaveBlob(blob, filename);
-     }
-     else {
-         var elem = window.document.createElement('a');
-         elem.href = window.URL.createObjectURL(blob);
-         elem.download = filename + '.txt';        
-         document.body.appendChild(elem);
-         elem.click();        
-         document.body.removeChild(elem);
-     }
-   }
+  const hourNumsStr = getHoursPerDayNumsStr();
    console.log('REMEMBER TO ADD DEBOUNCER FUNC LATER');
    createTextFile('Metrics: Deep work hours. Per day, week, month.', hourNumsStr);
  };
+ function createTextFile(filename, data) {
+  var blob = new Blob([data], {type: 'text'});
+  if(window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+  }
+  else {
+      var elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename + '.txt';        
+      document.body.appendChild(elem);
+      elem.click();        
+      document.body.removeChild(elem);
+  }
+}
+ function getHoursPerDayNumsStr() {
+  const days = document.getElementsByClassName('day');
+  const hourNums = [];
+  for (let i = 0; i < days.length; i++) {
+    hourNums.push(days[i].getElementsByClassName('hour').length);
+  }
+  const hourNumsStr = hourNums.join(','); 
+  return hourNumsStr;
+ }
 function createFillDataBtn() {
   const fillDataBtnContainer = (function createFillDataContainer() {
     const fillDataBtnContainer = document.createElement('div');
