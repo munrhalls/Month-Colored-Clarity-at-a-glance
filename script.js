0166550b2eb71db6c6e69baccfe69b2b40630e75
munrhalls <antarcticdepths@gmail.com>
2020-09-14 16:52:20 +0200

    abstract saving text file callback to func

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ function createMenu() {
   createFillDataBtn();
 }
 function createSaveTextFileBtn() {
-
   // components
     const SaveTextFileBtnContainer = (function createSaveTextFileBtnContainer() {
       const SaveTextFileBtnContainer = document.createElement('div');
@@ -95,32 +94,33 @@ function createSaveTextFileBtn() {
   SaveTextFileBtnContainer.appendChild(arrowGraphic);
   document.getElementById('menu').appendChild(SaveTextFileBtnContainer);
 
-  SaveTextFileBtnContainer.onclick = (function() {
-    // the only data it needs = hour ticks numbers 
-    const days = document.getElementsByClassName('day');
-    const hourNums = [];
-    for (let i = 0; i < days.length; i++) {
-      hourNums.push(days[i].getElementsByClassName('hour').length);
-    }
-    const hourNumsStr = hourNums.join(',');    
-    function createTextFile(filename, data) {
-      var blob = new Blob([data], {type: 'text'});
-      if(window.navigator.msSaveOrOpenBlob) {
-          window.navigator.msSaveBlob(blob, filename);
-      }
-      else {
-          var elem = window.document.createElement('a');
-          elem.href = window.URL.createObjectURL(blob);
-          elem.download = filename;        
-          document.body.appendChild(elem);
-          elem.click();        
-          document.body.removeChild(elem);
-      }
-    }
-    console.log('REMEMBER TO ADD DEBOUNCER FUNC LATER');
-    createTextFile('Metrics: Deep work hours. Per day, week, month.', hourNumsStr);
-  });
-} 
+  SaveTextFileBtnContainer.onclick = saveHoursAsTextFile;
+}
+function saveHoursAsTextFile() {
+  // the only data it needs = hour ticks numbers 
+   const days = document.getElementsByClassName('day');
+   const hourNums = [];
+   for (let i = 0; i < days.length; i++) {
+     hourNums.push(days[i].getElementsByClassName('hour').length);
+   }
+   const hourNumsStr = hourNums.join(',');    
+   function createTextFile(filename, data) {
+     var blob = new Blob([data], {type: 'text'});
+     if(window.navigator.msSaveOrOpenBlob) {
+         window.navigator.msSaveBlob(blob, filename);
+     }
+     else {
+         var elem = window.document.createElement('a');
+         elem.href = window.URL.createObjectURL(blob);
+         elem.download = filename;        
+         document.body.appendChild(elem);
+         elem.click();        
+         document.body.removeChild(elem);
+     }
+   }
+   console.log('REMEMBER TO ADD DEBOUNCER FUNC LATER');
+   createTextFile('Metrics: Deep work hours. Per day, week, month.', hourNumsStr);
+ };
 function createFillDataBtn() {
   const fillDataBtnContainer = (function createFillDataContainer() {
     const fillDataBtnContainer = document.createElement('div');
