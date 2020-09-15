54b6e11f7e5eb909b461e97160442d624bcb5f28
munrhalls <antarcticdepths@gmail.com>
2020-09-15 09:46:02 +0200

    EXCEL FEATURE: step - get months vertically

--- script.js
+++ script.js
@@ -269,6 +269,7 @@ function saveToExcel() {
   var month = document.getElementsByClassName('month')[0];
   var monthNamesDATA = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var weeks = month.getElementsByClassName('week'); 
+  const monthData = [];
 
   for (var i = 0; i < weeks.length; i++) {
     var weekObj = {};
@@ -281,10 +282,9 @@ function saveToExcel() {
       var hours = day.getElementsByClassName('hour');
     }
     monthData.push(weekObj)
-
   }
   var totalDeepWorkHours = document.getElementsByClassName('hour').length;
-  
+
   JSONToCSVConvertor([
     {'January': 'Week 1',
       'Monday': 8,
@@ -310,35 +310,49 @@ function saveToExcel() {
 };
 function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
   //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-  var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+  // var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
   var CSV = 'sep=,' + '\r\n\n';
-  //This condition will generate the Label/Header
-  if (ShowLabel) {
-      var row = "";
-      //This loop will extract the label from 1st index of on array
-      for (var index in arrData[0]) {
-          //Now convert each value to string and comma-seprated
-          row += index + ',';
-      }
-      row = row.slice(0, -1);
-      //append Label row with line break
-      CSV += row + '\r\n';
-  }
-  //1st loop is to extract each row
-  for (var i = 0; i < arrData.length; i++) {
-      var row = "";
-      //2nd loop will extract each column and convert it in string comma-seprated
-      for (var index in arrData[i]) {
-          row += '"' + arrData[i][index] + '",';
-      }
-      row.slice(0, row.length - 1);
-      //add a line break after each row
-      CSV += row + '\r\n';
-  }
-  if (CSV == '') {        
-      alert("Invalid data");
-      return;
-  }   
+  var row = "";
+  CSV+= 'January';
+  row.slice(0, -1);
+  CSV += row + '\r\n';
+  CSV+= 'February';
+  CSV += row + '\r\n';
+  CSV+= 'March';
+  CSV += row + '\r\n';
+
+
+
+
+  // //This condition will generate the Label/Header
+  // if (ShowLabel) {
+  //     var row = "";
+  //     //This loop will extract the label from 1st index of on array
+  //     for (var index in arrData[0]) {
+  //         //Now convert each value to string and comma-seprated
+  //         row += index + ',';
+  //     }
+  //     row = row.slice(0, -1);
+  //     //append Label row with line break
+  //     console.log(row);
+
+  //     CSV += row + '\r\n';
+  // }
+  // //1st loop is to extract each row
+  // for (var i = 0; i < arrData.length; i++) {
+  //     var row = "";
+  //     //2nd loop will extract each column and convert it in string comma-seprated
+  //     for (var index in arrData[i]) {
+  //         row += '"' + arrData[i][index] + '",';
+  //     }
+  //     row.slice(0, row.length - 1);
+  //     //add a line break after each row
+  //     CSV += row + '\r\n';
+  // }
+  // if (CSV == '') {        
+  //     alert("Invalid data");
+  //     return;
+  // }   
   //Generate a file name
   var fileName = "MyReport_";
   fileName += ReportTitle.replace(/ /g,"_");   
@@ -379,8 +393,6 @@ function createSidewaysTitle(sidewaysTitle, monthNum) {
     hours.style.backgroundColor = '#000';
     hours.style.textAlign = "center";
     hours.style.paddingRight = '6rem';
-    //CREATE SAVE TO EXCEL BUTTON
-    var monthData = [];
     //title
     var title = document.createElement('div');
     title.innerText = sidewaysTitle;
