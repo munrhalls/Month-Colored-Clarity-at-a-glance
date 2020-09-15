a2b97a3ae5c2d62254338d49072f06fc24d452e2
munrhalls <antarcticdepths@gmail.com>
2020-09-15 13:46:46 +0200

    set up testing

--- script.js
+++ script.js
@@ -308,9 +308,7 @@ function saveToExcel() {
     },
   ], 'ReportTitle', 'ShowLabel');
 };
-function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
-  //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-  // var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+function getCSV() {
   var CSV = 'sep=,' + '\r\n\n';
   CSV += 'January,';
   CSV += 'Monday,';
@@ -320,54 +318,23 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
   CSV += 'Friday,';
   CSV += 'Saturday,';
   CSV += 'Sunday,';
-
   CSV += '\r\n';
+  return CSV;
+}
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+  //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+  // var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+  var CSV = getCSV();
   CSV += 'Week 1,';
-  CSV += '\r\n';
-  CSV += 'Week 2,';
-  CSV += '\r\n';
-  CSV += 'Week 3,';
-  CSV += '\r\n';
-  CSV += 'Week 4,';
-  CSV += '\r\n';
-  CSV += 'February,';
+  CSV += '0,';
+  CSV += '0,';
+  CSV += '0,';
+  CSV += '0,';
+  CSV += '0,';
+  CSV += '0,';
+  CSV += '0,';
 
-
-
-  console.log(CSV)
-
-
-
-
-  // //This condition will generate the Label/Header
-  // if (ShowLabel) {
-  //     var row = "";
-  //     //This loop will extract the label from 1st index of on array
-  //     for (var index in arrData[0]) {
-  //         //Now convert each value to string and comma-seprated
-  //         row += index + ',';
-  //     }
-  //     row = row.slice(0, -1);
-  //     //append Label row with line break
-  //     console.log(row);
-
-  //     CSV += row + '\r\n';
-  // }
-  // //1st loop is to extract each row
-  // for (var i = 0; i < arrData.length; i++) {
-  //     var row = "";
-  //     //2nd loop will extract each column and convert it in string comma-seprated
-  //     for (var index in arrData[i]) {
-  //         row += '"' + arrData[i][index] + '",';
-  //     }
-  //     row.slice(0, row.length - 1);
-  //     //add a line break after each row
-  //     CSV += row + '\r\n';
-  // }
-  // if (CSV == '') {        
-  //     alert("Invalid data");
-  //     return;
-  // }   
+  console.log(CSV);
   //Generate a file name
   var fileName = "MyReport_";
   fileName += ReportTitle.replace(/ /g,"_");   
