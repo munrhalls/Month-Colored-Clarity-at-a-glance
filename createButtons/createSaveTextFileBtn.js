795b071328fee19093ec8ee29509b888a6964eef
munrhalls <antarcticdepths@gmail.com>
2020-09-24 00:11:35 +0200

    refactor

--- createButtons/createSaveTextFileBtn.js
+++ createButtons/createSaveTextFileBtn.js
@@ -51,8 +51,8 @@ function styleSaveIcon(saveIcon) {
       saveIcon.style.fill = '#fff';
   return saveIcon;
 }
-function createSaveTextFileBtn(){
-  function createSaveTextFileBtn() {
+function createSaveToTextFileBtn(){
+  function createSaveToTextFileBtn() {
     const container = createContainer();
     const SaveTextFileBtn = createBtnRENAME();
     const underLineGraphic = resourceCreateUnderLineGraphic();
@@ -66,39 +66,7 @@ function createSaveTextFileBtn(){
     container.appendChild(underLineGraphic);
     container.appendChild(arrowGraphic);
     document.getElementById('menu').appendChild(container);
-    container.onclick = saveHoursAsTextFile;
+    // container.onclick = saveToTextFile;
   }
-  function saveHoursAsTextFile() {
-    // the only data it needs = hour ticks numbers 
-    const hourNumsStr = getHoursPerDayNumsStr();
-     console.log('Debouncer, add. Reminder.');
-     createTextFile('Metrics: Deep work hours. Per day, week, month.', hourNumsStr);
-   }
-  function createTextFile(filename, data) {
-    var blob = new Blob([data], {type: 'text'});
-    if(window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-    }
-    else {
-        var elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename + '.txt';        
-        document.body.appendChild(elem);
-        elem.click();        
-        document.body.removeChild(elem);
-    }
-  }
-  function getHoursPerDayNumsStr() {
-    const days = document.getElementsByClassName('day');
-    const hourNums = [];
-    for (let i = 0; i < days.length; i++) {
-      hourNums.push(days[i].getElementsByClassName('hour').length);
-    }
-    const hourNumsStr = hourNums.join(','); 
-    return hourNumsStr;
-  }
-  const saveTextFileBtn = createSaveTextFileBtn();
-  return saveTextFileBtn;
-}
 
-export default createSaveTextFileBtn;
+export default createSaveToTextFileBtn;
