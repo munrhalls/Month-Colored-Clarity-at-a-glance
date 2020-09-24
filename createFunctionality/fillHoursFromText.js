860bbc411ff78d55430cfcab06749a3d9f66034c
munrhalls <antarcticdepths@gmail.com>
2020-09-24 09:29:18 +0200

    func mv

--- createFunctionality/fillHoursFromText.js
+++ createFunctionality/fillHoursFromText.js
@@ -46,5 +46,16 @@ function createHourTick() {
   hour.appendChild(svg);
   return hour;
 };
+function appendNumToHour(num, hour) {
+  var numEl = document.createElement('div');
+  numEl.style.color = '#000';
+  numEl.style.fontWeight = "bold";
+  numEl.style.fontSize = '1.6rem';
+  numEl.style.position = 'absolute';
+  numEl.style.left = '15px';
+  numEl.style.top = '0.1rem';
+  numEl.innerText = num;
+  hour.appendChild(numEl);
+}
 
 export { fillHoursFromText, clearHoursFromTheDay, appendHoursToDay, appendHour, createHourTick }
