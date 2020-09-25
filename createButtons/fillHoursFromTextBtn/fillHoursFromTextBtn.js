60532e47c6292b2e05dd8f89c4ce10554808fa41
munrhalls <antarcticdepths@gmail.com>
2020-09-25 14:52:15 +0200

    refactor

--- createButtons/fillHoursFromTextBtn/fillHoursFromTextBtn.js
+++ createButtons/fillHoursFromTextBtn/fillHoursFromTextBtn.js
@@ -1,12 +1,12 @@
 import container from './container.js';
 import dropTextFileLabel from './dropTextFileLabel.js';
-import dropTextFileBtn from './dropTextFileBtn.js';
+import dropTextFileInput from './dropTextFileInput.js';
 import bottomText from './bottomText.js';
 import column from './column.js';
 import { arrowGraphic, chartIcon, fillIcon, underLineGraphic } from './icons.js';
 
 function fillHoursFromTextBtn() {
-    column.appendChild(dropTextFileBtn);
+    column.appendChild(dropTextFileInput);
     column.appendChild(dropTextFileLabel);
     column.appendChild(fillIcon)
     column.appendChild(bottomText);
--- createButtons/saveToTextFile/createButton_saveToTextFile.js
+++ createButtons/saveToTextFile/createButton_saveToTextFile.js
@@ -1,17 +1,18 @@
 import saveToTextFile from '../../functionality/saveToTextFile/saveToTextFile.js';
-import { underLineGraphic, arrowGraphic, saveIcon } from '../../createResources/resources.js';
 import container from './container.js'
-import saveTextFileBtn from './saveToTextFileBtn.js'
+import saveTextToFileBtn from './saveToTextFileBtn.js'
 import bottomText from './bottomText.js'
 import styleSaveIcon from './styleSaveIcon.js'
+import { underLineGraphic, arrowGraphic, saveIcon } from '../../createResources/resources.js';
 
 function createButton_saveToTextFile() {
   styleSaveIcon(saveIcon);
-  container.appendChild(saveTextFileBtn);
   container.appendChild(saveIcon);
   container.appendChild(bottomText);
   container.appendChild(underLineGraphic);
   container.appendChild(arrowGraphic);
+
+  container.appendChild(saveTextToFileBtn);
   document.getElementById('menu').appendChild(container);
   container.onclick = saveToTextFile;
 }
