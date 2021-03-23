c4991a25c69debd8be4b859855983e463abdca0a
munrhalls <antarcticdepths@gmail.com>
2021-03-23 18:09:30 +0100

    unfinished makeover branch - save

--- script.js
+++ script.js
@@ -14,4 +14,5 @@ window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
   dominoAssembleCodeBlocks();
 });
+// unfinished makeover
 
