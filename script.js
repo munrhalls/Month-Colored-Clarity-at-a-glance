00b2885d764de95818c943f04c9a4801182822db
munrhalls <antarcticdepths@gmail.com>
2021-04-01 18:22:11 +0200

    create and style about page content

--- script.js
+++ script.js
@@ -90,12 +90,38 @@ function createAboutPage() {
     
     aboutModal.style.display = 'flex';
     aboutModal.style.justifyContent = 'center';
+    
+    const textContainer = document.createElement('div');
+    textContainer.innerText = 'ABOUT PAGE';
+    textContainer.style.padding = '3rem';
+    textContainer.style.fontSize = '3rem';
+    textContainer.style.color = "white";
+    
+    function createParagraph(container, message, marginTop, fontSize, bold) {
+      const paragraph = document.createElement('div');
+      paragraph.innerText = message;
+      paragraph.style.padding = '1rem 0';
+      paragraph.style.marginTop = marginTop || 0;
+      paragraph.style.color = "white";
+      paragraph.style.fontSize = fontSize || '1.5rem';
+      paragraph.style.fontWeight = bold || 'light';
+      container.appendChild(paragraph);
+    }
+    createParagraph(textContainer, 'This is a TOOL to LOG HOURS OF DEEP WORK.', '1rem');
+    createParagraph(textContainer, 'NO DATABASE REQUIREMENT.');
+    createParagraph(textContainer, 'All hour logs data is managed by via simple textfile.');
+    createParagraph(textContainer, 'Literally just that. Windows notepad file. Word file. Any file ending with ".txt"');
+    createParagraph(textContainer, '');
+    createParagraph(textContainer, '30 SECONDS TUTORIAL (LITERALLY):', '1.5rem', '2rem', 'bold');
+    createParagraph(textContainer, '1. Add hours worked by CLICKING +/- BUTTONS in given day\'s column.');
+    createParagraph(textContainer, '2. Click "SAVE TO TEXT FILE" button, in the top menu.');
+    createParagraph(textContainer, '3. Prompt will appear, asking to open or save text file. Choose "Save file." and save it to desktop or wherever you can find it easily. \n (The file is automatically named "DEEP WORK HOURS LOG.txt", with time and date.)');
+    createParagraph(textContainer, '4. Close the page. You\'ll see all the hours data disappeared.');
+    createParagraph(textContainer, '5. Re-open the page. Click DROP TEXT FILE. Prompt will appear, allowing you to browse files. Find the text file. Click it. Press enter or click ok. ALL YOUR LOGGED HOURS DATA RE-APPEARS!');
 
-    const text = document.createElement('div');
-    text.innerText = 'ABOUT PAGE';
-    text.style.padding = '3rem';
-    text.style.color = "white";
-    aboutModal.appendChild(text);
+
+
+    aboutModal.appendChild(textContainer);
     body.appendChild(aboutModal);
     console.log(aboutModal);
   }
