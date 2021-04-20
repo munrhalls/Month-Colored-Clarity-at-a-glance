3edaa42ee2ce8b5d55be074881aad820298de5e7
munrhalls <antarcticdepths@gmail.com>
2021-04-20 10:44:34 +0200

    add +/- btns & hour tick

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const date = new Date();
-  const year = date.getFullYear();
-  const yearDATA = [];
-  for (let i = 1; i <= 12; i++) {
-    // Each loop equals:
-    // 1. get number of days in a month.
-    const month = new Date(year, i, 0);
-    let monthDATA = new Array();
-    monthDATA.monthName = month.toLocaleString('default', { month: 'long' });
-    const numDaysInMonth = month.getDate();
-    // 2. get weeks with proper days as string, mark each week by comma
-    let weeksByCommaString = '';
-    for (let j = 1; j <= numDaysInMonth; j++) {
-      const day = new Date(year, i - 1, j);
-      const dayName = day.toLocaleDateString('en-EN', { weekday: 'long' });
-      weeksByCommaString += dayName === 'Sunday' ? dayName + ',' : dayName + ' ';
-    }
-    // 3. gather weeks from the string, as arrays of days, push each into month array
-    const weeks = weeksByCommaString.split(',');
-    weeks.pop();
-    let count = 1;
-    weeks.forEach(function(week){
-      const weekInDays = week.split(' ');
-      weekInDays.weekName = 'Week ' + count; 
-      monthDATA.push(weekInDays);
-      count++;
-    })
-    // 4. push month array, filled with month's week arrays with days inside, into year array
-    yearDATA.push(monthDATA);
+const year = date.getFullYear();
+const yearDATA = [];
+for (let i = 1; i <= 12; i++) {
+  // Each loop equals:
+  // 1. get number of days in a month.
+  const month = new Date(year, i, 0);
+  let monthDATA = new Array();
+  monthDATA.monthName = month.toLocaleString('default', { month: 'long' });
+  const numDaysInMonth = month.getDate();
+  // 2. get weeks with proper days as string, mark each week by comma
+  let weeksByCommaString = '';
+  for (let j = 1; j <= numDaysInMonth; j++) {
+    const day = new Date(year, i - 1, j);
+    const dayName = day.toLocaleDateString('en-EN', { weekday: 'long' });
+    weeksByCommaString += dayName === 'Sunday' ? dayName + ',' : dayName + ' ';
   }
-  // console.log(yearDATA)
-  
+  // 3. gather weeks from the string, as arrays of days, push each into month array
+  const weeks = weeksByCommaString.split(',');
+  weeks.pop();
+  let count = 1;
+  weeks.forEach(function (week) {
+    const weekInDays = week.split(' ');
+    weekInDays.weekName = 'Week ' + count;
+    monthDATA.push(weekInDays);
+    count++;
+  })
+  // 4. push month array, filled with month's week arrays with days inside, into year array
+  yearDATA.push(monthDATA);
+}
+// console.log(yearDATA)
+
 
 window.addEventListener('load', function () {
   console.log('This function is executed once the page is fully loaded');
@@ -69,6 +69,22 @@ function createHoursLog() {
         dayEl.style.height = '60vh';
         dayEl.style.width = '3rem';
         dayEl.style.borderLeft = '1px solid #000';
+
+        let addHourBtn = document.createElement('div');
+        addHourBtn.innerText = '+';
+        addHourBtn.onclick = function() {
+          alert('hey')
+        }
+        let minusHourBtn = document.createElement('div');
+        minusHourBtn.innerText = '-';
+        minusHourBtn.onclick = function() {
+          
+        }
+        let hourTick = resourceCreateHourTick();
+        dayEl.appendChild(addHourBtn)
+        dayEl.appendChild(minusHourBtn)
+        dayEl.appendChild(hourTick)
+
         weekContainerEl.appendChild(dayEl);
         console.log(day);
       }
@@ -980,6 +996,28 @@ function createHourTick() {
   hour.appendChild(svg);
   return hour;
 };
+function resourceCreateHourTick() {
+  var hour = document.createElement('div');
+  hour.className = "hour";
+  hour.style.backgroundColor = "#000071";
+  hour.style.padding = "7px 0";
+  hour.style.border = "1px solid darkblue";
+  hour.style.position = 'relative';
+  // deep work hour count - visual representer
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("width", "100%");
+  svg.setAttribute("height", "28");
+  svg.setAttribute("width", "28");
+  svg.setAttribute('display', 'block');
+  svg.style.margin = "auto";
+  var tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  tick.setAttributeNS(null, 'd', 'M95.833,0C42.991,0,0,42.99,0,95.833s42.991,95.834,95.833,95.834s95.833-42.991,95.833-95.834S148.676,0,95.833,0z   M150.862,79.646l-60.207,60.207c-2.56,2.56-5.963,3.969-9.583,3.969c-3.62,0-7.023-1.409-9.583-3.969l-30.685-30.685  c-2.56-2.56-3.97-5.963-3.97-9.583c0-3.621,1.41-7.024,3.97-9.584c2.559-2.56,5.962-3.97,9.583-3.97c3.62,0,7.024,1.41,9.583,3.971  l21.101,21.1l50.623-50.623c2.56-2.56,5.963-3.969,9.583-3.969c3.62,0,7.023,1.409,9.583,3.969  C156.146,65.765,156.146,74.362,150.862,79.646z');
+  tick.style.scale = "0.14";
+  tick.style.x = "50%";
+  svg.appendChild(tick);
+  hour.appendChild(svg);
+  return hour;
+}
 function appendNumToHour(num, hour) {
   var numEl = document.createElement('div');
   numEl.style.color = '#000';
