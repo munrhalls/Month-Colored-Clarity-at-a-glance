6b8e7b65330790ce845bfd33f4bbcc1d75bed271
munrhalls <antarcticdepths@gmail.com>
2021-05-19 19:12:07 +0200

    get the border arrow concept working

--- script.js
+++ script.js
@@ -88,6 +88,9 @@ window.addEventListener('load', function () {
       arrowSensorL.style.height = '5px';
       arrowSensorL.style.width = '5px';
       arrowSensorL.style.border = '5px solid black';
+      arrowSensorL.style.borderLeft = '0px solid black';
+      arrowSensorL.style.borderBottom = '2px solid white';
+      arrowSensorL.style.borderTop = '1px solid white';
       childEl.appendChild(arrowSensorL);
       let arrowSensorR = document.createElement('div');
       // on left n - 1
