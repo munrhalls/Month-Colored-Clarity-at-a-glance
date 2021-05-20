a27c8f0e458906897ed4f111a8905b6c2760889a
munrhalls <antarcticdepths@gmail.com>
2021-05-20 15:17:41 +0200

    fix bug

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ window.addEventListener('load', function () {
   let containerRoot = document.getElementById('containerRoot');
   /// CONTAINER TOPBAR
   let containerTopBar = document.createElement('div');
-
   let topBar = createTopBar();
   containerTopBar.appendChild(topBar);
   /// CONTAINER TOPBAR CONCLUDE
@@ -46,105 +45,112 @@ window.addEventListener('load', function () {
   let containerMain = document.createElement('containerMain');
   containerMain.id = 'containerMain';
   containerMain.style.display = 'flex';
-  containerRoot.appendChild(containerMain);
-  //// CONTAINER VISUALS
-  let containerVisuals = document.createElement('div');
-  containerVisuals.style.height = '45vh';
-  containerVisuals.style.width = '100%';
-  let hourLog = createHoursLog();
-  function carouselify(HTMLCollection, arrDistanceTop, arrDistanceLeft, arrSize) {
-    let length = HTMLCollection.length;
-    console.log(length);
-    for (let i = 0; i < length; i++) {
-      let childEl = HTMLCollection[i];
-      console.log(childEl);
-      childEl.style.position = 'relative';
-      childEl.style.display = 'none';
-      // add event sensors
-      let sensorsContainer = document.createElement('div');
-      sensorsContainer.style.position = 'absolute';
-      sensorsContainer.style.top = arrDistanceTop;
-      sensorsContainer.style.left = arrDistanceLeft;
-      sensorsContainer.style.display = 'flex';
-      // created an arrow of its own! (can re-use later);
-      let arrowLength = arrSize;
-      let borderBottom = (arrowLength / 1.25) / arrowLength;
-      let borderTop = (arrowLength / 1.25) / arrowLength;
-      let arrowLengthToRem = arrowLength + 'rem';
-      let borderBottomToRem = borderBottom + 'rem';
-      let borderTopToRem = borderTop + 'rem';
-      let sensorsContainerWidth = arrowLength * 2.5;
-      let sensorsContainerWidthToRem = sensorsContainerWidth + 'rem';
-      sensorsContainer.style.width = sensorsContainerWidthToRem;
-      sensorsContainer.style.justifyContent = 'space-between';
-      if (i !== 0) {
-        let arrowSensorL = document.createElement('div');
-        arrowSensorL.style.height = '5px';
-        arrowSensorL.style.width = '5px';
-        arrowSensorL.style.border = arrowLengthToRem + ' solid black';
-        arrowSensorL.style.borderLeft = '0 solid black';
-        arrowSensorL.style.borderBottom = borderBottomToRem + ' solid white';
+  function createMain() {
+    //// CONTAINER VISUALS
+    let main = document.createElement('div');
+    let containerVisuals = document.createElement('div');
+    containerVisuals.style.height = '45vh';
+    containerVisuals.style.width = '100%';
+    let hourLog = createHoursLog();
+    function carouselify(HTMLCollection, arrDistanceTop, arrDistanceLeft, arrSize) {
+      let length = HTMLCollection.length;
+      console.log(length);
+      for (let i = 0; i < length; i++) {
+        let childEl = HTMLCollection[i];
+        console.log(childEl);
+        childEl.style.position = 'relative';
+        childEl.style.display = 'none';
+        // add event sensors
+        let sensorsContainer = document.createElement('div');
+        sensorsContainer.style.position = 'absolute';
+        sensorsContainer.style.top = arrDistanceTop;
+        sensorsContainer.style.left = arrDistanceLeft;
+        sensorsContainer.style.display = 'flex';
+        // created an arrow of its own! (can re-use later);
+        let arrowLength = arrSize;
+        let borderBottom = (arrowLength / 1.25) / arrowLength;
+        let borderTop = (arrowLength / 1.25) / arrowLength;
+        let arrowLengthToRem = arrowLength + 'rem';
+        let borderBottomToRem = borderBottom + 'rem';
+        let borderTopToRem = borderTop + 'rem';
+        let sensorsContainerWidth = arrowLength * 2.5;
+        let sensorsContainerWidthToRem = sensorsContainerWidth + 'rem';
+        sensorsContainer.style.width = sensorsContainerWidthToRem;
+        sensorsContainer.style.justifyContent = 'space-between';
+        if (i !== 0) {
+          let arrowSensorL = document.createElement('div');
+          arrowSensorL.style.height = '5px';
+          arrowSensorL.style.width = '5px';
+          arrowSensorL.style.border = arrowLengthToRem + ' solid black';
+          arrowSensorL.style.borderLeft = '0 solid black';
+          arrowSensorL.style.borderBottom = borderBottomToRem + ' solid white';
+          // 4/12 0,3333
+          arrowSensorL.style.borderTop = borderTopToRem + ' solid white';
+          // 1/12 0,0833
+          arrowSensorL.onclick = function (e) {
+            childEl.style.display = 'none';
+            let index = i - 1;
+            let prevEl = HTMLCollection[index];
+            prevEl.style.display = 'block';
+            console.log(prevEl);
+          }
+          sensorsContainer.appendChild(arrowSensorL);
+        }
+
+        let arrowSensorR = document.createElement('div');
+        arrowSensorR.style.height = '5px';
+        arrowSensorR.style.width = '5px';
+        arrowSensorR.style.border = arrowLengthToRem + ' solid black';
+        arrowSensorR.style.borderRight = '0 solid black';
+        arrowSensorR.style.borderBottom = borderBottomToRem + ' solid white';
         // 4/12 0,3333
-        arrowSensorL.style.borderTop = borderTopToRem + ' solid white';
-        // 1/12 0,0833
-        arrowSensorL.onclick = function (e) {
-          childEl.style.display = 'none';
-          let index = i - 1;
-          let prevEl = HTMLCollection[index];
-          prevEl.style.display = 'block';
-          console.log(prevEl);
+        arrowSensorR.style.borderTop = borderTopToRem + ' solid white';
+        if (i + 1 < length) {
+          arrowSensorR.onclick = function (e) {
+            childEl.style.display = 'none';
+            let index = i + 1;
+            let nextEl = HTMLCollection[index];
+            nextEl.style.display = 'block';
+            console.log(nextEl);
+          }
+        } else {
+          arrowSensorR.style.display = 'none';
         }
-        sensorsContainer.appendChild(arrowSensorL);
-      }
+        sensorsContainer.appendChild(arrowSensorR);
+        childEl.appendChild(sensorsContainer);
 
-      let arrowSensorR = document.createElement('div');
-      arrowSensorR.style.height = '5px';
-      arrowSensorR.style.width = '5px';
-      arrowSensorR.style.border = arrowLengthToRem + ' solid black';
-      arrowSensorR.style.borderRight = '0 solid black';
-      arrowSensorR.style.borderBottom = borderBottomToRem + ' solid white';
-      // 4/12 0,3333
-      arrowSensorR.style.borderTop = borderTopToRem + ' solid white';
-      if (i + 1 < length) {
-        arrowSensorR.onclick = function (e) {
-          childEl.style.display = 'none';
-          let index = i + 1;
-          let nextEl = HTMLCollection[index];
-          nextEl.style.display = 'block';
-          console.log(nextEl);
-        }
-      } else {
-        arrowSensorR.style.display = 'none';
+        // on left n - 1`
+        // on right n + 1
+        // set current to hidden
+        // set new to visible
       }
-      sensorsContainer.appendChild(arrowSensorR);
-      childEl.appendChild(sensorsContainer);
-
-      // on left n - 1`
-      // on right n + 1
-      // set current to hidden
-      // set new to visible
+      HTMLCollection[0].style.display = 'block';
     }
-    HTMLCollection[0].style.display = 'block';
+    carouselify(hourLog.children, '0.5rem', '7rem', 3);
+    let topHalf = document.createElement('div');
+    topHalf.style.height = '45vh';
+    topHalf.style.width = '100%';
+    let botHalf = document.createElement('div');
+    botHalf.style.height = '45vh';
+    botHalf.style.width = '100%';
+    botHalf.appendChild(hourLog)
+    containerVisuals.appendChild(topHalf);
+    containerVisuals.appendChild(botHalf);
+    containerMain.appendChild(containerVisuals);
+    //// CONTAINER VISUALS CONCLUDE
+    //// CONTAINER MENU
+    // let containerMenu = document.createElement('div');
+    // let menu = createMenu();
+    // containerMenu.appendChild(menu);
+    // containerMain.appendChild(containerMenu);
+    return main;
+    //// CONTAINER MENU CONCLUDE
   }
-  carouselify(hourLog.children, '0.5rem', '7rem', 3);
-  let topHalf = document.createElement('div');
-  topHalf.style.height = '45vh';
-  topHalf.style.width = '100%';
-  let botHalf = document.createElement('div');
-  botHalf.style.height = '45vh';
-  botHalf.style.width = '100%';
-  botHalf.appendChild(hourLog)
-  containerVisuals.appendChild(topHalf);
-  containerVisuals.appendChild(botHalf);
-  containerMain.appendChild(containerVisuals);
-  //// CONTAINER VISUALS CONCLUDE
-  //// CONTAINER MENU
-  let containerMenu = document.createElement('div');
-  let menu = createMenu();
-  containerMenu.appendChild(menu);
-  containerMain.appendChild(containerMenu);
-  //// CONTAINER MENU CONCLUDE
+  let main = createMain();
+  containerMain.appendChild(main);
+  containerRoot.appendChild(containerMain);
   /// CONTAINER MAIN CONCLUDE
+
   /// CONTAINER BOTTOMBAR
   let bottomBar = document.createElement('div');
   let copyrightNote = createCopyrightNote();
