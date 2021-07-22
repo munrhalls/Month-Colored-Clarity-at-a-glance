bd66bb8d18ea6f276cbccd767af6a6070d1f3fce
munrhalls <antarcticdepths@gmail.com>
2021-07-22 21:01:15 +0200

    interesting color effect

--- script.js
+++ script.js
@@ -752,27 +752,60 @@ function app() {
           function inter_OBSERVE_scrollIntoView() {
             const barList = document.getElementsByClassName('projectBar');
             const addedBar = barList[barList.length - 1];
-            const callback = function (e) {
-              const symbolLines = document.getElementsByClassName('projectBarSymbolLine');
-              const bar = e[0].target;
-              const barsList = document.getElementsByClassName('projectBar');
-              const index = Array.prototype.indexOf.call(barsList, bar);
-              const symbolLine = symbolLines[index];
-              resetAllSymbolLines();
-              function resetAllSymbolLines() {
-                for (let i = 0; i < symbolLines.length; i++) {
-                  const symbolLine = symbolLines[i];
-                  symbolLine.style.height = '1px';
+            const numSteps = 20.0;
+            let boxElement;
+            let prevRatio = 0.0;
+            let increasingColor = "rgba(40, 40, 190, ratio)";
+            let decreasingColor = "rgba(190, 40, 40, ratio)";
+            function handleIntersect(entries, observer) {
+              entries.forEach((entry) => {
+                if (entry.intersectionRatio > prevRatio) {
+                  entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+                } else {
+                  entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
                 }
-              }
-              symbolLine.style.height = '3px';
+
+                prevRatio = entry.intersectionRatio;
+              });
             }
+
+            // const handleIntersect = function (e) {
+            //   const symbolLines = document.getElementsByClassName('projectBarSymbolLine');
+            //   const bar = e[0].target;
+            //   const barsList = document.getElementsByClassName('projectBar');
+            //   const index = Array.prototype.indexOf.call(barsList, bar);
+            //   const symbolLine = symbolLines[index];
+
+            //   // resetAllSymbolLines();
+            //   // function resetAllSymbolLines() {
+            //   //   for (let i = 0; i < symbolLines.length; i++) {
+            //   //     const symbolLine = symbolLines[i];
+            //   //     symbolLine.style.height = '1px';
+            //   //   }
+            //   // }
+            //   symbolLine.style.height = '3px';
+            // }
             let options = {
               root: document.getElementsByClassName('projectBars')[0],
               rootMargin: '0px',
-              threshold: 1.0
+              threshold: buildThresholdList()
             }
-            let observer = new IntersectionObserver(callback, options);
+
+            function buildThresholdList() {
+              let thresholds = [];
+              let numSteps = 20;
+
+              for (let i = 1.0; i <= numSteps; i++) {
+                let ratio = i / numSteps;
+                thresholds.push(ratio);
+              }
+
+              thresholds.push(0);
+              return thresholds;
+            }
+
+
+            let observer = new IntersectionObserver(handleIntersect, options);
             observer.observe(addedBar);
           }
 
