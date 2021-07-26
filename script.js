6c7f7321feeae4b6d25902351915c8f431177deb
munrhalls <antarcticdepths@gmail.com>
2021-07-26 14:55:15 +0200

    z index based card-like displayi n the project bars hour blocks

--- script.js
+++ script.js
@@ -964,6 +964,8 @@ function app() {
                 newHourBlock.style.left = blocksNum * 1.5 + 'rem';
                 newHourBlock.style.zIndex = blocksNum;
                 newHourBlock.style.backgroundColor = bgColor;
+                newHourBlock.style.textAlign = 'left';
+                newHourBlock.style.paddingLeft = '0.15rem';
                 return newHourBlock;
               }
               function append() {
